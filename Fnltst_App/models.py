746da85d33a6c439d7ec4171f466fbafe3c1effd
Python-Django-Student <90149099+Python-Django-Student@users.noreply.github.com>
2022-01-07 16:14:56 +0530

    Update models.py

--- Fnltst_App/models.py
+++ Fnltst_App/models.py
@@ -3,7 +3,6 @@ import datetime
 
 # Create your models here.
 class Task(models.Model):
-    img=models.ImageField(upload_to="images")
     name=models.CharField(max_length=100)
     rate=models.IntegerField()
     datetime=models.DateTimeField(default=datetime.datetime.today)
